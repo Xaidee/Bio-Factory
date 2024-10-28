1c5e64e5608f699947429a9e126b4d19b3c551be
Elenterius <w.elenterius@gmail.com>
2024-10-28 23:49:22 +0100

    feat: add tributes for Potion, Builder Tea and Honey fluid

--- src/main/java/com/github/elenterius/biofactory/integration/biomancy/BiomancyIntegration.java
+++ src/main/java/com/github/elenterius/biofactory/integration/biomancy/BiomancyIntegration.java
@@ -4,10 +4,26 @@ import com.github.elenterius.biofactory.init.ModFluids;
 import com.github.elenterius.biomancy.api.nutrients.Nutrients;
 import com.github.elenterius.biomancy.api.nutrients.fluid.FluidNutrients;
 import com.github.elenterius.biomancy.api.nutrients.fluid.FluidToFuelConversion;
+import com.github.elenterius.biomancy.api.tribute.MilliTribute;
+import com.github.elenterius.biomancy.api.tribute.MobEffectTribute;
+import com.github.elenterius.biomancy.api.tribute.MobEffectTribute.Builder;
+import com.github.elenterius.biomancy.api.tribute.Tribute;
+import com.github.elenterius.biomancy.api.tribute.Tributes;
+import com.github.elenterius.biomancy.api.tribute.fluid.FluidToTributeConversion;
+import com.github.elenterius.biomancy.api.tribute.fluid.FluidTributes;
+import com.simibubi.create.AllFluids;
+import com.simibubi.create.AllItems;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraftforge.fluids.FluidStack;
 
 public final class BiomancyIntegration {
+
+	public static final int GLASS_BOTTLE_AMOUNT = 250;
+	public static final int BUCKET_AMOUNT = 1000;
 
 	public static final FluidToFuelConversion FLUID_TO_FUEL_CONVERSION = new FluidToFuelConversion() {
 		public int getFuelMultiplier(FluidStack resource) {
@@ -19,10 +35,34 @@ public final class BiomancyIntegration {
 		}
 	};
 
-	private BiomancyIntegration() {}
+	public static final FluidToTributeConversion POTION_FLUID_TO_TRIBUTE_CONVERSION = resource -> {
+		CompoundTag tag = resource.getOrCreateTag();
+
+		Builder builder = MobEffectTribute.builder();
+		PotionUtils.getAllEffects(tag).forEach(builder::addEffect);
+		MobEffectTribute effectTribute = builder.build();
+
+		return MilliTribute.convertToMilliUnit(effectTribute, GLASS_BOTTLE_AMOUNT);
+	};
+
+	private static final Tribute BUILDER_TEA_TRIBUTE = Tributes.combineTributes(
+		MobEffectTribute.builder().addEffect(MobEffects.DIG_SPEED, 0, 3 * 60 * 20, 1f).build(),
+		Tributes.getTribute(Items.MILK_BUCKET.getDefaultInstance())
+	);
+	private static final MilliTribute BUILDER_TEA_MILLI_TRIBUTE = MilliTribute.convertToMilliUnit(BUILDER_TEA_TRIBUTE, GLASS_BOTTLE_AMOUNT);
+	private static final MilliTribute HONEY_MILLI_TRIBUTE = MilliTribute.convertToMilliUnit(Tributes.getTribute(Items.HONEY_BOTTLE.getDefaultInstance()), GLASS_BOTTLE_AMOUNT);
+
+	private BiomancyIntegration() {
+	}
 
 	public static void onPostSetup() {
-		FluidNutrients.registerFuel(ModFluids.NUTRIENTS_TYPE, FLUID_TO_FUEL_CONVERSION);
+		FluidNutrients.register(ModFluids.NUTRIENTS_FLUID, FLUID_TO_FUEL_CONVERSION);
+
+		Tributes.register(AllItems.BUILDERS_TEA.get(), BUILDER_TEA_TRIBUTE);
+
+		FluidTributes.register(AllFluids.TEA.get(), resource -> BUILDER_TEA_MILLI_TRIBUTE);
+		FluidTributes.register(AllFluids.POTION.get(), POTION_FLUID_TO_TRIBUTE_CONVERSION);
+		FluidTributes.register(AllFluids.HONEY.get(), resource -> HONEY_MILLI_TRIBUTE);
 	}
 
 	public static int convertTofluidAmount(ItemStack resource) {
