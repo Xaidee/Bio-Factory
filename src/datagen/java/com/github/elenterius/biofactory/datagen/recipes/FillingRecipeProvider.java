7d50cc1328c98c34a4a43cb8eefcb23fcb091988
Elenterius <w.elenterius@gmail.com>
2024-11-09 00:11:45 +0100

    feat: add alternative recipe for rich-soil from Farmer's Delight

--- src/datagen/java/com/github/elenterius/biofactory/datagen/recipes/FillingRecipeProvider.java
+++ src/datagen/java/com/github/elenterius/biofactory/datagen/recipes/FillingRecipeProvider.java
@@ -5,6 +5,7 @@ import com.github.elenterius.biofactory.init.ModItems;
 import com.github.elenterius.biofactory.item.NutrientsBottleItem;
 import com.simibubi.create.AllRecipeTypes;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Items;
 
 public class FillingRecipeProvider extends ProcessingRecipeProvider {
@@ -19,6 +20,12 @@ public class FillingRecipeProvider extends ProcessingRecipeProvider {
 			.require(ModFluids.NUTRIENTS_FLUID.get(), NutrientsBottleItem.FLUID_AMOUNT)
 			.require(Items.GLASS_BOTTLE)
 			.output(ModItems.NUTRIENTS_BOTTLE.get())
+		);
+
+		create("rich_soil", recipeBuilder -> recipeBuilder.whenModLoaded("farmersdelight")
+			.require(ModFluids.NUTRIENTS_FLUID.get(), NutrientsBottleItem.FLUID_AMOUNT * 2)
+			.require(Items.DIRT)
+			.output(1, new ResourceLocation("farmersdelight", "rich_soil"), 1)
 		);
 	}
 
